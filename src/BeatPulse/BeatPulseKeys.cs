4a3df57c94772e00ce9b698067c716abc1237d0b
Unai Zorrilla Castro <uzorrilla@plainconcepts.com>
2018-02-19 19:21:56 +0100

    Issue #1 ( https://github.com/Xabaril/BeatPulse/issues/1 )

--- src/BeatPulse/BeatPulseKeys.cs
+++ src/BeatPulse/BeatPulseKeys.cs
@@ -3,7 +3,7 @@
     public class BeatPulseKeys
     {
         public const string BEATPULSE_DEFAULT_PATH = "hc";
-        public const string BEATPULSE_SELFT_SEGMENT = "self";
+        public const string BEATPULSE_SELF_SEGMENT = "_self";
         public const string BEATPULSE_PATH_SEGMENT_NAME = "segment";
     }
 }
