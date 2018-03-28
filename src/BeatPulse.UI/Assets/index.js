3d139b1e16fea116f60f5a83dd59fdd124eb19a0
Carlos Landeras <carlos.landeras@gmail.com>
2018-03-28 13:39:14 +0200

    Initial javascript UI bootstrapping with health check

--- src/BeatPulse.UI/Assets/index.js
+++ src/BeatPulse.UI/Assets/index.js
@@ -1,1 +1,69 @@
-﻿
+﻿(function(BeatPulse){
+
+    //Temporal code and url mapping
+
+    BeatPulse.client  = BeatPulse.client || {};
+    BeatPulse.client._request = sendRequest;     
+    BeatPulse.client.checkHealth = () => {
+        return BeatPulse.client._request({
+            url: window.location.origin + "/health",
+            method: "get"
+        });
+    }
+
+    function HttpError(xhrError){
+        return {
+            response: xhrError.response,
+            status: xhrError.status,
+            statusCode: xhrError.statusCode
+        }
+    }
+
+    function HttpResponse(xhrResponse){
+        return {
+            status: xhrResponse.status,
+            statusText: xhrResponse.statusText, 
+            response: xhrResponse.response || xhrResponse.responseText
+        }
+    }
+
+    function sendRequest(request) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(request.method,request.url, true);
+        
+            xhr.setRequestHeader("X-Request-Client", "XMLHttpClient");
+            xhr.setRequestHeader("Content-type", request.contentType || "application/json");
+        
+            xhr.onload = () => {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve(new HttpResponse(xhr));
+                } else {
+                    reject(new HttpError(xhr));
+                }
+            }
+            xhr.onerror = () => {
+                reject(new HttpError(xhr));
+            }
+        
+            xhr.ontimeout = () => {
+                reject(new HttpError(xhr));
+            }
+            xhr.send(request.content || "");
+        });
+    }
+
+    if(!("BeatPulse" in window)){
+        window.BeatPulse = BeatPulse;
+    }
+
+})
+(window.BeatPulse || {});
+
+//temporal check
+var container = document.getElementById("container")
+BeatPulse.client.checkHealth().then( response => {
+    container.innerText = JSON.stringify(response);
+}).catch(err =>   container.innerText = JSON.stringify(err));
+
+
