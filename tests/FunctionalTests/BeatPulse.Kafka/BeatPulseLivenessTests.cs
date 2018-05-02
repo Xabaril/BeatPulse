00749ad244839302d89b5bb5f12c491910c8ffb6
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2018-05-02 09:43:26 +0200

    Add Kafka liveness

---
+++ tests/FunctionalTests/BeatPulse.Kafka/BeatPulseLivenessTests.cs
@@ -0,0 +1,86 @@
+﻿using BeatPulse;
+using FluentAssertions;
+using FunctionalTests.Base;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalTests.BeatPulse.Kafka
+{
+    [Collection("execution")]
+    public class kafka_liveness_should
+    {
+        private readonly ExecutionFixture _fixture;
+
+        public kafka_liveness_should(ExecutionFixture fixture)
+        {
+            _fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        }
+
+        [Fact]
+        public async Task return_false_if_kafka_is_unavailable()
+        {
+            var config = new Dictionary<string, object>()
+            {
+                { "bootstrap.servers", "localhost:0000"},
+                { "message.send.max.retries", 0 },
+                { "default.topic.config", new Dictionary<string, object>()
+                    {
+                        { "message.timeout.ms", 5000 }
+                    }
+                }
+            };
+
+            var webHostBuilder = new WebHostBuilder()
+               .UseStartup<DefaultStartup>()
+               .UseBeatPulse()
+               .ConfigureServices(services =>
+               {
+                   services.AddBeatPulse(context =>
+                   {
+                       context.AddKafka(config);
+                   });
+               });
+
+            var server = new TestServer(webHostBuilder);
+
+            var response = await server.CreateRequest($"{BeatPulseKeys.BEATPULSE_DEFAULT_PATH}")
+                .GetAsync();
+
+            response.StatusCode
+                .Should().Be(HttpStatusCode.ServiceUnavailable);
+        }
+
+        [Fact]
+        public async Task return_true_if_kafka_is_available()
+        {
+            var config = new Dictionary<string, object>()
+            {
+                { "bootstrap.servers", "localhost:9092"}
+            };
+
+            var webHostBuilder = new WebHostBuilder()
+               .UseStartup<DefaultStartup>()
+               .UseBeatPulse()
+               .ConfigureServices(services =>
+               {
+                   services.AddBeatPulse(context =>
+                   {
+                       context.AddKafka(config);
+                   });
+               });
+
+            var server = new TestServer(webHostBuilder);
+
+            var response = await server.CreateRequest($"{BeatPulseKeys.BEATPULSE_DEFAULT_PATH}")
+                .GetAsync();
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
