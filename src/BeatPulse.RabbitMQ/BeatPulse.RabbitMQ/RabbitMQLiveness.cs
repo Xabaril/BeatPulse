ca5e34df025f52cc6ce71906ad3f0df88814270a
Matt Channer <matt.channer@referencepoint.co.uk>
2018-05-10 08:30:03 +0100

    BeatPulse.RabbitMQ project added along with functional and unit tests

---
+++ src/BeatPulse.RabbitMQ/BeatPulse.RabbitMQ/RabbitMQLiveness.cs
@@ -0,0 +1,53 @@
+﻿using Rmq = RabbitMQ;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using BeatPulse.Core;
+
+using Microsoft.AspNetCore.Http;
+
+namespace BeatPulse.RabbitMQ
+{
+    public class RabbitMQLiveness : IBeatPulseLiveness
+    {
+        private readonly string _rabbitMqConnectionString;
+
+        public RabbitMQLiveness(string rabbitMqConnectionString)
+        {
+            _rabbitMqConnectionString = rabbitMqConnectionString ?? throw new ArgumentNullException(nameof(rabbitMqConnectionString));
+        }
+
+        public string Name => nameof(RabbitMQLiveness);
+
+        public string DefaultPath => "rabbitmq";
+
+        public Task<(string, bool)> IsHealthy(HttpContext context, bool isDevelopment, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+                var factory = new Rmq.Client.ConnectionFactory();
+                factory.Uri = new Uri(_rabbitMqConnectionString);
+
+                using (var connection = factory.CreateConnection())
+                {
+                    if (connection.IsOpen)
+                    {
+                        return Task.FromResult((BeatPulseKeys.BEATPULSE_HEALTHCHECK_DEFAULT_OK_MESSAGE, true));
+                    }
+                }
+
+                return Task.FromResult((BeatPulseKeys.BEATPULSE_HEALTHCHECK_DEFAULT_ERROR_MESSAGE, false));
+            }
+            catch (Exception ex)
+            {
+                var message = !isDevelopment
+                    ? string.Format(BeatPulseKeys.BEATPULSE_HEALTHCHECK_DEFAULT_ERROR_MESSAGE, Name)
+                    : $"Exception {ex.GetType().Name} with message ('{ex.Message}')";
+
+                return Task.FromResult((message, false));
+            }
+        }
+    }
+}
