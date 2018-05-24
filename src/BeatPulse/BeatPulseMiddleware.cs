70de175d7b44b1c8b779de8913c3bac860ea6841
Unai Zorrilla Castro <unai@plainconcepts.com>
2018-05-24 18:49:53 +0200

    Set BeatPulseMiddleware as public class

--- src/BeatPulse/BeatPulseMiddleware.cs
+++ src/BeatPulse/BeatPulseMiddleware.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace BeatPulse
 {
-    class BeatPulseMiddleware
+    public class BeatPulseMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly TemplateMatcher _templateMatcher;
