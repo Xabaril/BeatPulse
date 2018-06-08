96ce18b639dbab0e00aec3093348f4af9a26a01d
Carlos Landeras <carlos.landeras@gmail.com>
2018-06-08 23:33:46 +0200

    Add Amazon AWS DynamoDB liveness

---
+++ src/BeatPulse.AWSDynamoDB/BeatPulseContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BeatPulse.AWS.DynamoDB;
+using BeatPulse.Core;
+using System;
+
+namespace BeatPulse
+{
+    public static class BeatPulseContextExtensions
+    {
+        public static BeatPulseContext AddAWSDynamoDB(this BeatPulseContext context, Action<AWSDynamoDBOptions> setupOptions, string defaultPath = "awsdynamodb")
+        {
+            var options = new AWSDynamoDBOptions();
+            setupOptions(options);
+
+            context.AddLiveness(new AWSDynamoDBLiveness(options, defaultPath));
+            return context;
+        }
+    }
+}
