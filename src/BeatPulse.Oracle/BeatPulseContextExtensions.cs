821035f9b7864926eaf04c70bf4f2bf912ad7fdf
Carlos Landeras <carlos.landeras@gmail.com>
2018-08-08 23:34:07 +0200

    Oracle liveness and functional tests

---
+++ src/BeatPulse.Oracle/BeatPulseContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BeatPulse.Core;
+using BeatPulse.Oracle;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeatPulse
+{
+    public static class BeatPulseContextExtensions
+    {
+        public static BeatPulseContext AddOracle(this BeatPulseContext context, string connectionString, string name = nameof(OracleLiveness), string defaultPath = "oracle")
+        {
+            return context.AddLiveness(name, setup => {
+                setup.UsePath(defaultPath);                
+                setup.UseLiveness(new OracleLiveness(connectionString));
+            });
+        }
+    }
+}
